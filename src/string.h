7a974102b821015540ee9a18cf1faa2625199afb
Andrew Kesterson <andrew@aklabs.net>
2016-03-27 13:23:46 -0700

    Fix failing tests for stringlib

--- src/string.h
+++ src/string.h
@@ -7,6 +7,6 @@ size_t strlen(char *ptr);
 int strncat(char *dest, char *src, size_t n);
 void *memset(void *s, int c, size_t n);
 void *memcpy(void *dest, void *src, size_t n);
-int strcmp(const char *s1, const char *s2);
+int strcmp(char *s1, char *s2);
 
 #endif /* _STRING_H_ */
